451a295fb86dac50311fac6ea8cf0c0c5ef93b68
Nitesh <niteshmali132002@gmail.com>
2024-12-09 13:51:58 +0530

    Modified background worker to handle async methods

--- js/background.js
+++ js/background.js
@@ -1,26 +1,20 @@
 import { GistManager } from './gist_manager.js';
 
-chrome.runtime.onMessage.addListener((message, sendResponse) => {
-    console.log("Message received.");
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "uploadGist") {
-        console.log("For gist uploading");
         const { token, description, files, isPublic } = message.payload;
 
-        try {
-            const gistManager = new GistManager(token);
-            gistManager
-                .uploadGist(description, files, isPublic)
-                .then((response) => {
-                    sendResponse({ success: true, data: response });
-                })
-                .catch((error) => {
-                    console.error("Gist Upload Error:", error.message);
-                    sendResponse({ success: false, error: error.message });
-                });
-        } catch (error) {
-            console.error("Initialization Error:", error.message);
-            sendResponse({ success: false, error: error.message });
-        }
+        const handleGistUpload = async () => {
+            try {
+                const gistManager = new GistManager(token);
+                const response = await gistManager.uploadGist(description, files, isPublic);
+                sendResponse({ success: true, data: response });
+            } catch (error) {
+                console.error("Gist Upload Error:", error.message);
+                sendResponse({ success: false, error: error.message });
+            }
+        };
+        handleGistUpload();
 
         return true;
     }
